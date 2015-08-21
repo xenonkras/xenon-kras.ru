0e0e2385da30148f64c54a0dcac866deaf0b5e04
xenonkras <vladzaets@gmail.com>
2015-08-21 10:19:08 +0700

    update

--- custom.js
+++ custom.js
@@ -1,20 +1,29 @@
-$('#new-order-form button').click(function() {
-  var data_html = $('#new-order-form').serialize();
-  $('#new-order-form input').attr('disabled', true);
-  $('#new-order-form button').attr('disabled', true);
-  $('#new-order-form button').text('Отправляем...');
-  $.ajax({
-    type: 'POST',
-    url: 'http://notificator.fenixlz.ru/send',
-    data: data_html,
-    success: function() {
-      $('#new-order-form button').text('Отправлено');
-    },
-    error: function(argument) {
-      alert("Ошибка! Не удалось отправить заказ.\nПопробуйте еще раз, либо позвоните нам 8 923 278 0555")
-      $('#new-order-form input').attr('disabled', false);
-      $('#new-order-form button').attr('disabled', false);
-      $('#new-order-form button').text('Попробовать еще раз');
-    }
+$(document).ready(function() {
+  $('#new-order-form button').click(function() {
+    var data_html = $('#new-order-form').serialize();
+    $('#new-order-form input').attr('disabled', true);
+    $('#new-order-form button').attr('disabled', true);
+    $('#new-order-form button').text('Отправляем...');
+    $.ajax({
+      type: 'POST',
+      url: 'http://notificator.fenixlz.ru/send',
+      data: data_html,
+      success: function() {
+        $('#new-order-form button').text('Отправлено');
+      },
+      error: function(argument) {
+        alert("Ошибка! Не удалось отправить заказ.\nПопробуйте еще раз, либо позвоните нам 8 923 278 0555")
+        $('#new-order-form input').attr('disabled', false);
+        $('#new-order-form button').attr('disabled', false);
+        $('#new-order-form button').text('Попробовать еще раз');
+      }
+    })
   })
+
+  var date = new Date(),
+      month = date.getMonth(),
+      day = date.getDate(),
+      months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'],
+      promo_text = 'Акция с 1 по ' + day + ' ' + months[month] + '!';
+  $('#promo-date').text(promo_text);
 })
