7997c2fea1536ab8b1dd9d232afb1464226854dd
xenonkras <vladzaets@gmail.com>
2015-08-25 10:33:40 +0700

    update

--- custom.js
+++ custom.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
   $('#new-order-form button').click(function() {
-    var data_html = $('#new-order-form').serialize();
-    $('#new-order-form input').attr('disabled', true);
-    $('#new-order-form button').attr('disabled', true);
-    $('#new-order-form button').text('Отправляем...');
-    $.ajax({
-      type: 'POST',
-      url: 'http://notificator.fenixlz.ru/send',
-      data: data_html,
-      success: function() {
-        $('#new-order-form button').text('Отправлено');
-      },
-      error: function(argument) {
-        alert("Ошибка! Не удалось отправить заказ.\nПопробуйте еще раз, либо позвоните нам 8 923 278 0555")
-        $('#new-order-form input').attr('disabled', false);
-        $('#new-order-form button').attr('disabled', false);
-        $('#new-order-form button').text('Попробовать еще раз');
-      }
-    })
+    var name = $('#new-order-form input[name="name"]').val(),
+        phone = $('#new-order-form input[name="phone"]').val();
+    if (name == '' || phone == '') {
+      alert('НЕОБХОДИМО ВВЕСТИ ВАШЕ ИМЯ И ТЕЛЕФОН');
+    } else {
+      var data_html = $('#new-order-form').serialize();
+      $('#new-order-form input').attr('disabled', true);
+      $('#new-order-form button').attr('disabled', true);
+      $('#new-order-form button').text('Отправляем...');
+      $.ajax({
+        type: 'POST',
+        url: 'http://notificator.fenixlz.ru/send',
+        data: data_html,
+        success: function() {
+          $('#new-order-form button').text('Отправлено');
+        },
+        error: function(argument) {
+          alert("Ошибка! Не удалось отправить заказ.\nПопробуйте еще раз, либо позвоните нам 8 923 278 0555")
+          $('#new-order-form input').attr('disabled', false);
+          $('#new-order-form button').attr('disabled', false);
+          $('#new-order-form button').text('Попробовать еще раз');
+        }
+      })
+    }
   })
 
   var date = new Date(),
